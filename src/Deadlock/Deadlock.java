4fb69485a2a2e4aaa62ee932a4de1f562314a284
Jorge Sierra <jsierra.siete@gmail.com>
2017-04-01 18:10:13 +0100

    Ejemplo deadlock

---
+++ src/Deadlock/Deadlock.java
@@ -0,0 +1,37 @@
+
+public class Deadlock {  
+	public static void main(String[] args) {  
+		final String resource1 = "Resource one";  
+		final String resource2 = "Resource two";  
+		
+		// t1 tries to lock resource1 then resource2  
+		Thread t1 = new Thread() {  
+			public void run() {  
+				synchronized (resource1) {  
+					System.out.println("Thread 1: locked resource 1");  
+					try { Thread.sleep(100);} catch (Exception e) {}  
+					System.out.println("Thread 1: Waiting for resource 2...");
+					synchronized (resource2) {  
+						System.out.println("Thread 1: locked resource 2");  
+					}  
+				}  
+			}  
+		};  
+		
+		// t2 tries to lock resource2 then resource1  
+		Thread t2 = new Thread() {  
+			public void run() {  
+				synchronized (resource2) {  
+					System.out.println("Thread 2: locked resource 2");  
+					try { Thread.sleep(100);} catch (Exception e) {}
+					System.out.println("Thread 1: Waiting for resource 1...");
+					synchronized (resource1) {  
+						System.out.println("Thread 2: locked resource 1");  
+					}  
+				}  
+			}  
+		};  
+		t1.start();  
+		t2.start();
+	}  
+}  
